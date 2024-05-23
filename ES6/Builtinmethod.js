8180eb28107adedba9ae8dea933f5b26a3a48b97
vina <vinaneosoft@gmail.com>
2024-05-23 16:01:41 +0530

    commit

--- ES6/Builtinmethod.js
+++ ES6/Builtinmethod.js
@@ -6,13 +6,24 @@ salaries.forEach((value,index)=>console.log(value+" "+index));
 console.log("----------");
 salaries.forEach((value,index, ref)=>console.log(value+" "+index+ " "+ref));
 // find method
-
-
-
+// first salary > 70000
+console.log("----------");
+const matchingsalary=salaries.find(salary=>salary>70000)
+console.log(matchingsalary);
 // filter
-
-
+salaries > 70000
+console.log("----------");
+const matchingsalaries=salaries.filter(salary=>salary>70000)
+console.log(matchingsalaries);
+// display salaries > 70000 using forEach
+console.log("----------");
+salaries.filter(salary=>salary>70000).forEach(sal=>console.log(sal));
 // map
-
-
+// increment each salary by 5000
+console.log("----------");
+let incrementedsalaries=salaries.map(salary=>salary+5000)
+console.log(incrementedsalaries);
+console.log(salaries);
+console.log("----------");
+salaries.map(salary=>salary+5000).forEach(salary=>console.log(salary))
 // reduce
