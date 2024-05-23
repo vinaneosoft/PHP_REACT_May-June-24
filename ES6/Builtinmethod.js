cc5dd168e17c65afb9cec46cbed81375b65a4402
vina <vinaneosoft@gmail.com>
2024-05-23 15:50:55 +0530

    Initial commit

---
+++ ES6/Builtinmethod.js
@@ -0,0 +1,18 @@
+let salaries=[45000,56000,45000,67000,78000,89000,56000,98000,55000];
+// forEach methods
+salaries.forEach(value=>console.log(value));
+console.log("----------");
+salaries.forEach((value,index)=>console.log(value+" "+index));
+console.log("----------");
+salaries.forEach((value,index, ref)=>console.log(value+" "+index+ " "+ref));
+// find method
+
+
+
+// filter
+
+
+// map
+
+
+// reduce
