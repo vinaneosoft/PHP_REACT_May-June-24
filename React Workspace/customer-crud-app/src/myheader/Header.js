7f0ab85594ba81dc493f8d18f13126f2213643c7
vina <vinaneosoft@gmail.com>
2024-05-24 18:44:02 +0530

    commit

--- React Workspace/customer-crud-app/src/myheader/Header.js
+++ React Workspace/customer-crud-app/src/myheader/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 export function Header(){
     return(
         <header className="myHeader">
-                <h1>CUSTOMER CRUD PROJECT</h1>
+                <h1 className="text-success">CUSTOMER CRUD PROJECT</h1>
         </header>
     );
 }
