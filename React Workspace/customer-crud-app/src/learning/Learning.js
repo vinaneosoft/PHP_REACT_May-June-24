9d5d33747e0f803b36e962c920daf756d02f229c
vina <vinaneosoft@gmail.com>
2024-05-27 18:18:25 +0530

    commit

--- React Workspace/customer-crud-app/src/learning/Learning.js
+++ React Workspace/customer-crud-app/src/learning/Learning.js
@@ -1,4 +1,5 @@
 // u can declare javascript variables here : var, let, const
+import { useState } from "react";
 import  dish  from "../Resources/kadhaipaneer.jpg";
 import  './Learning.css';
 
@@ -7,45 +8,61 @@ extract publc path from  "process.env.PUBLIC_URL" */
 export function Learning(){
 
     // u can declare javascript variables here : var, let , const
-    const subHeading="customer operations"
+    let subHeading="customer operations"
     const imageObj={
         imgPath:"https://www.funfoodfrolic.com/wp-content/uploads/2020/12/Kadhai-Paneer-Thumbnail-1170x1170.jpg",
         message:"Paneer Kadhai",
-        height:300,
-        width:400  
+        height:100,
+        width:100  
     }
     const imageObj2={
         imgPath:process.env.PUBLIC_URL+"/kadhaipaneer.jpg",
         message:"Paneer Kadhai",
-        height:300,
-        width:400  
+        height:100,
+        width:100  
     }
     const imageObj3={
         imgPath:dish,
         message:"Paneer Kadhai",
-        height:300,
-        width:400  
+        height:100,
+        width:100  
     }
     const imageObj4={
         imgPath:require('../Resources/kadhaipaneer.jpg'),
         message:"Paneer Kadhai Dish",
-        height:300,
-        width:400  
+        height:100,
+        width:100  
     }
 
     // react style
     const styleObj={
-        height:"500px",
-        width:"600px",
+        height:"100px",
+        width:"100px",
         border:"5px dashed blue",
         backgroundColor:"red"
     }
 
     const styleArray=['rounded-circle', 'border','border-5', "border-danger"]
     const classes=styleArray.join(" ");
+/* message is variable available in component,  state of message ="" 
+ state change in variable  : component re rendered*/
+
+  /* if u want to make a state change in variable we have to use hook in react
+  useState */
+   let [message, changeState]=useState("");
+
+    function getMessage(){
+        console.log("in function...");
+        message="Welcome to my online Kitchen" // this wont work in react
+        changeState("Welcome to my online Kitchen");
+       // return message;
+    }
+ 
+
     return (
     <div className="container">
         <h4>{subHeading}</h4> {/*Data binding*/}
+        {/* Attribute Binding */}
         <figure>
             <img src="https://www.funfoodfrolic.com/wp-content/uploads/2020/12/Kadhai-Paneer-Thumbnail-1170x1170.jpg" alt="kadhai paneer" height="400" width="500"></img>
             <figcaption>Kadhai Paneer</figcaption>
@@ -59,10 +76,14 @@ export function Learning(){
             <figcaption>{imageObj3.message}</figcaption>
             <img src={imageObj4.imgPath} alt={imageObj4.message} height={imageObj4.height} width={imageObj4.width}></img>
             <figcaption>{imageObj4.message}</figcaption>
-            <img style={styleObj} src={imageObj4.imgPath}></img>
+            <img style={styleObj} src={imageObj4.imgPath} alt="dish"></img>
             <hr></hr>
-            <img className={classes} src={imageObj4.imgPath}></img>
+            <img className={classes} src={imageObj4.imgPath} alt="dish" height={imageObj4.height} width={imageObj4.width}></img>
         </figure>
+        <hr></hr>
+        {/* <b>{getMessage()}</b> */}
+        <button className="btn btn-info" onClick={getMessage}>MESSAGE</button>
+       <b>{message}</b>
      </div>
     );
 }
