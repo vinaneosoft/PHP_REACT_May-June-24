47b8d5fe8c2f5cbfaf8051cf0a99590f9e846e40
vina <vinaneosoft@gmail.com>
2024-05-27 15:43:43 +0530

    commit

--- React Workspace/customer-crud-app/src/App.js
+++ React Workspace/customer-crud-app/src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import './App.css';
 import { Header } from "./myheader/Header";
 import { Footer } from "./Footer/Footer";
+import { Learning } from "./learning/Learning";
 
 function App() {
   return (
     <React.Fragment>  
         <Header></Header>
+        <Learning></Learning>
         <Footer></Footer>
     </React.Fragment>  
   );
---
+++ React Workspace/customer-crud-app/src/learning/Learning.js
@@ -0,0 +1,13 @@
+// u can declare javascript variables here : var, let, const
+
+export function Learning(){
+
+    // u can declare javascript variables here : var, let , const
+    const subHeading="customer operations"
+    return (
+        <div className="container">
+            <h4>{subHeading}</h4>
+        
+        </div>
+    );
+}
