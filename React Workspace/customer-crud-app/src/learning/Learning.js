8b778d6a607a4758783fef945fb258f9033fc24b
vina <vinaneosoft@gmail.com>
2024-05-27 15:52:10 +0530

    commit

--- React Workspace/customer-crud-app/src/learning/Learning.js
+++ React Workspace/customer-crud-app/src/learning/Learning.js
@@ -5,9 +5,12 @@ export function Learning(){
     // u can declare javascript variables here : var, let , const
     const subHeading="customer operations"
     return (
-        <div className="container">
-            <h4>{subHeading}</h4>
-        
-        </div>
+    <div className="container">
+        <h4>{subHeading}</h4> {/*Data binding*/}
+        <figure>
+            <img src="https://www.funfoodfrolic.com/wp-content/uploads/2020/12/Kadhai-Paneer-Thumbnail-1170x1170.jpg" alt="kadhai paneer" height="400" width="500"></img>
+            <figcaption>Kadhai Paneer</figcaption>
+        </figure>
+     </div>
     );
 }
