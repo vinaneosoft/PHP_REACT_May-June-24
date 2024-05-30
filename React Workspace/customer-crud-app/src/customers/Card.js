b0e38d18c567c0e590ce8e344e7cab3f6e4d6a00
vina <vinaneosoft@gmail.com>
2024-05-30 11:26:51 +0530

    commit

---
+++ React Workspace/customer-crud-app/src/customers/Card.js
@@ -0,0 +1,22 @@
+export function Card({customer}){
+    return (
+        <div key={customer.customerId} className="card bg-info" style={{width :"18rem"}} >
+        <div className="card-body">
+            <h5 className="card-title"><span>#</span>{customer.customerId}</h5>
+        </div>
+        <img src={require("../Resources/"+customer.customerImage)} className="card-img-top" alt="..."></img>
+        <div className="card-body">
+            <h5 className="card-title">{customer.customerName}</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+            <li className="list-group-item"><b><small>Contact number: </small></b>{customer.customerContact}</li>
+            <li className="list-group-item"><b><small>Email Id: </small></b>{customer.customerEmail}</li>
+            <li className="list-group-item"><b><small>Username: </small></b>{customer.username}</li>
+        </ul>
+        <div className="card-body">
+            <button className="btn btn-info">EDIT</button>
+            <button className="btn btn-danger">DELETE</button>
+        </div>
+    </div>   
+    );
+}
--- React Workspace/customer-crud-app/src/customers/Customers.js
+++ React Workspace/customer-crud-app/src/customers/Customers.js
@@ -1,3 +1,4 @@
+import { Card } from "./Card";
 import { MyCustomer } from "./MyCustomer";
 
 export function Customers(){
@@ -17,26 +18,7 @@ export function Customers(){
             <td>{customer.password}</td>
         </tr>);
 
-    const cardElements=customers.map(customer=> 
-    <div key={customer.customerId} className="card bg-info" style={{width :"18rem"}} >
-        <div className="card-body">
-            <h5 className="card-title"><span>#</span>{customer.customerId}</h5>
-        </div>
-        <img src={require("../Resources/"+customer.customerImage)} className="card-img-top" alt="..."></img>
-        <div className="card-body">
-            <h5 className="card-title">{customer.customerName}</h5>
-        </div>
-        <ul className="list-group list-group-flush">
-            <li className="list-group-item"><b><small>Contact number: </small></b>{customer.customerContact}</li>
-            <li className="list-group-item"><b><small>Email Id: </small></b>{customer.customerEmail}</li>
-            <li className="list-group-item"><b><small>Username: </small></b>{customer.username}</li>
-        </ul>
-        <div className="card-body">
-            <button className="btn btn-info">EDIT</button>
-            <button className="btn btn-danger">DELETE</button>
-        </div>
-    </div>   
-) 
+    const cardElements=customers.map(customer=><Card customer={customer}></Card> ) 
     return(
         <>
        <h4>CUSTOMER DETAILS</h4>
