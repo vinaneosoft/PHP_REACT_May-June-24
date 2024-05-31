12ec89fa5276840a12cb87bd9249d379e674b3b5
vina <vinaneosoft@gmail.com>
2024-05-31 15:26:10 +0530

    commit

--- React Workspace/customer-crud-app/src/learning/NonvegMenus.js
+++ React Workspace/customer-crud-app/src/learning/NonvegMenus.js
@@ -1,4 +1,11 @@
+import { useContext } from "react";
+import { MyContext } from "../App";
+
 export function NonvegMenus(props){
+
+    const data=useContext(MyContext);
+    console.log(data);
+    
     const nonvegMenus=["Surmai Fish Fry", "Chicken Curry","Mutton Fry", "Egg Masala", "Chicken Bhuna"]
     const liElements=nonvegMenus.map((ele,i)=><li key={i}>{ele}</li>);
     return(
