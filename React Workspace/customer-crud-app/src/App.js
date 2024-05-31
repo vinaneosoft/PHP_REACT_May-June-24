b45c8b4a5e5c6416ce45d0a239df40374588ca22
vina <vinaneosoft@gmail.com>
2024-05-31 15:24:21 +0530

    cmmit

--- React Workspace/customer-crud-app/src/App.js
+++ React Workspace/customer-crud-app/src/App.js
@@ -9,17 +9,24 @@ import { Customers } from "./customers/Customers";
 import { Register } from "./register/Register";
 import { Login } from "./login/Login";
 
+
+export const MyContext=React.createContext();
+
+
+
 function App() {
-  
+  let bgColor="yellow";
   return (
     <React.Fragment>  
-        <Header></Header>
-       {/*  <Hooks></Hooks> */}
-       {/*  <Login></Login> */}
-       <Register></Register>
-        {/* <Customers></Customers>
-        <JSXExpressions></JSXExpressions> */}
-        <Footer></Footer>
+      <MyContext.Provider value={bgColor}>
+          <Header></Header>
+          {/*  <Hooks></Hooks> */}
+          {/*  <Login></Login> */}
+          {/*  <Register></Register> */}
+          {/* <Customers></Customers>*/}
+          <JSXExpressions></JSXExpressions> 
+          <Footer></Footer>
+      </MyContext.Provider>
     </React.Fragment>  
   );
 }
