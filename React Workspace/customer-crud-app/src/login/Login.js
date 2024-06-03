7bdc59d3c74172e6e6dd8d522575e6fbe123df36
vina <vinaneosoft@gmail.com>
2024-06-03 17:44:38 +0530

    commit

--- React Workspace/customer-crud-app/src/login/Login.js
+++ React Workspace/customer-crud-app/src/login/Login.js
@@ -1,21 +1,29 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function Login(){
-    let uname="ABC";
-    const inputnode=useRef();
+    let uname;
+    let password;
+    const userNode=useRef();
+    const passNode=useRef();
     const paranode=useRef();
+
+    const [errorMessage, setMessage]=useState("");
     function collectData(ev){
         ev.preventDefault();
-       // console.log(inputnode.current);
-      //  console.log(inputnode.current.value);
-      //  console.log(inputnode.current.type);
-        console.log(uname);
-        uname=inputnode.current.value;
-        console.log(uname); // use this internally
+        uname=userNode.current.value;
+        password=passNode.current.value;
+        loginCheck();
+    }
+    function loginCheck(){
+        if(uname=="admin" && password=="admin123")
+            {
+
+            }
+        else
+            setMessage("Incorrect admin username or password...");
     }
     function getPara(){
         console.log(paranode.current);
-        console.log(inputnode.current);
         console.log(paranode.current.textContent);
     }   
     useEffect(()=>{
@@ -26,14 +34,15 @@ export function Login(){
         <>
         <h4>LOGIN FORM</h4>
         <b>{uname}</b>
+       <b style={{color:"red"}}><small>{errorMessage}</small></b>
         <form onSubmit={collectData}>
             <div className="mb-3">
                 <label htmlFor="uname" className="form-label">USERNAME</label>
-                <input id="uname" name="username"  type="text" className="form-control" ref={inputnode} ></input>
+                <input id="uname" name="username"  type="text" className="form-control" ref={userNode} ></input>
             </div>
             <div className="mb-3">
                 <label htmlFor="pass" className="form-label">PASSWORD</label>
-                <input id="pass" name="password" type="password" className="form-control" ></input>
+                <input id="pass" name="password" type="password" className="form-control"  ref={passNode}  ></input>
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
